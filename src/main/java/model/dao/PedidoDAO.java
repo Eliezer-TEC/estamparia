a83d3122fd4279fe9ca49bcc32e18f6eb218f2ef
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-06-07 21:29:28 -0300

    for para preencher id do pedido na camisa

--- src/main/java/model/dao/PedidoDAO.java
+++ src/main/java/model/dao/PedidoDAO.java
@@ -4,20 +4,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import model.vo.Camisa;
 import model.vo.Pedido;
 
 public class PedidoDAO {
 	public Pedido inserir(Pedido novoPedido) {
 		Connection conexao = Banco.getConnection();
-		String sql = " INSERT INTO PEDIDO(STATUS_PEDIDO, ID_PESSOA, ID_CAMISA) " + " VALUES (?,?,?) ";
+		String sql = " INSERT INTO PEDIDO(STATUS_PEDIDO, ID_PESSOA) " + " VALUES (?,?) ";
 		PreparedStatement stmt = Banco.getPreparedStatementWithPk(conexao, sql);
 		try {
 			stmt.setInt(1, novoPedido.getSituacaoPedido().getValor());
-			stmt.setInt(2, novoPedido.getIdPessoa());
-			stmt.setInt(3, novoPedido.getIdCamisa());
+			stmt.setInt(2, novoPedido.getPessoa().getId());
 			
-
 			stmt.execute();
 
 			// Preencher o id gerado no banco no objeto
@@ -26,6 +26,12 @@ public class PedidoDAO {
 				novoPedido.setId(resultado.getInt(1));
 			}
 
+			CamisaDAO camisaDao = new CamisaDAO();
+			for(Camisa c: novoPedido.getCamisas()) {
+				c.setIdPedido(novoPedido.getId());
+				camisaDao.inserir(c);
+			}
+			
 		} catch (SQLException e) {
 			System.out.println("Erro ao inserir novo pedido.");
 			System.out.println("Erro: " + e.getMessage());
@@ -33,4 +39,6 @@ public class PedidoDAO {
 
 		return novoPedido;
 	}
+	
+		
 }
