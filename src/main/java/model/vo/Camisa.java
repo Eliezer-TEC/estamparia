3d1d4def0ddbd0f19b96958cb4f1e612cefa32c5
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-06-07 21:27:31 -0300

    camisa possui idPedido

--- src/main/java/model/vo/Camisa.java
+++ src/main/java/model/vo/Camisa.java
@@ -3,58 +3,57 @@ package model.vo;
 import java.sql.Blob;
 
 public class Camisa {
+	
 	private Integer id;
 	private String tamanho;
 	private String cor;
 	private Blob estampa;
-
+	private Integer idPedido;
+	
 	public Camisa() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-
-	public Camisa(Integer id, String tamanho, String cor, Blob estampa) {
+	public Camisa(Integer id, String tamanho, String cor, Blob estampa, Integer idPedido) {
 		super();
 		this.id = id;
 		this.tamanho = tamanho;
 		this.cor = cor;
 		this.estampa = estampa;
+		this.idPedido = idPedido;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getTamanho() {
 		return tamanho;
 	}
-
 	public void setTamanho(String tamanho) {
 		this.tamanho = tamanho;
 	}
-
 	public String getCor() {
 		return cor;
 	}
-
 	public void setCor(String cor) {
 		this.cor = cor;
 	}
-
 	public Blob getEstampa() {
 		return estampa;
 	}
-
 	public void setEstampa(Blob estampa) {
 		this.estampa = estampa;
 	}
-
-	@Override
-	public String toString() {
-		return "Camisa [id=" + id + ", tamanho=" + tamanho + ", cor=" + cor + ", estampa=" + estampa + "]";
+	public Integer getIdPedido() {
+		return idPedido;
 	}
-
+	public void setIdPedido(Integer idPedido) {
+		this.idPedido = idPedido;
+	}
+	
+	
+	
 }
