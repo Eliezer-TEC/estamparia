f2452b0341425df20ab5694ff1539d4612fa4b68
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-06-14 21:46:37 -0300

    painel novo pedido

--- src/main/java/view/PainelNovoPedido.java
+++ src/main/java/view/PainelNovoPedido.java
@@ -1,6 +1,11 @@
 package view;
 
 import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
 
 public class PainelNovoPedido extends JPanel {
 
@@ -8,6 +13,31 @@ public class PainelNovoPedido extends JPanel {
 	 * Create the panel.
 	 */
 	public PainelNovoPedido() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,
+				FormSpecs.RELATED_GAP_COLSPEC,
+				FormSpecs.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNewLabel = new JLabel("Novo Pedido");
+		add(lblNewLabel, "4, 4");
+		
+		JLabel lblNewLabel_1 = new JLabel("Tamanho");
+		add(lblNewLabel_1, "4, 8");
+		
+		JLabel lblNewLabel_2 = new JLabel("Cor");
+		add(lblNewLabel_2, "4, 10");
 
 	}
 
