e15e09c7549cd1af5d7334b052b4393dede66ba8
Louisi de Mello Tomasi Dalazen <78367618+Louisii@users.noreply.github.com>
2023-06-20 22:36:02 -0300

    fazendo tela cadastro de pedido

--- src/main/java/view/PainelNovoPedido.java
+++ src/main/java/view/PainelNovoPedido.java
@@ -6,6 +6,9 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class PainelNovoPedido extends JPanel {
 
@@ -17,8 +20,18 @@ public class PainelNovoPedido extends JPanel {
 				FormSpecs.RELATED_GAP_COLSPEC,
 				FormSpecs.DEFAULT_COLSPEC,
 				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,},
+				ColumnSpec.decode("max(42dlu;default)"),
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(200dlu;default):grow"),
+				FormSpecs.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(285dlu;default)"),},
 			new RowSpec[] {
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
+				FormSpecs.RELATED_GAP_ROWSPEC,
+				FormSpecs.DEFAULT_ROWSPEC,
 				FormSpecs.RELATED_GAP_ROWSPEC,
 				FormSpecs.DEFAULT_ROWSPEC,
 				FormSpecs.RELATED_GAP_ROWSPEC,
@@ -34,10 +47,25 @@ public class PainelNovoPedido extends JPanel {
 		add(lblNewLabel, "4, 4");
 		
 		JLabel lblNewLabel_1 = new JLabel("Tamanho");
-		add(lblNewLabel_1, "4, 8");
+		add(lblNewLabel_1, "4, 8, right, default");
+		
+		JComboBox comboBox = new JComboBox();
+		add(comboBox, "6, 8, fill, default");
 		
 		JLabel lblNewLabel_2 = new JLabel("Cor");
-		add(lblNewLabel_2, "4, 10");
+		add(lblNewLabel_2, "4, 10, right, default");
+		
+		JComboBox comboBox_1 = new JComboBox();
+		add(comboBox_1, "6, 10, fill, default");
+		
+		JButton btnProcurarImagem = new JButton("Procurar Imagem");
+		add(btnProcurarImagem, "6, 12");
+		
+		JLabel lblNomeImg = new JLabel("");
+		add(lblNomeImg, "6, 14");
+		
+		JLabel lblItensDoPedido = new JLabel("Itens do Pedido");
+		add(lblItensDoPedido, "4, 16");
 
 	}
 
