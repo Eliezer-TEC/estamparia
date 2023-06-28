fb2c0bc008c8b51873884bd7378a81e21ec46813
ahnaoRaquel <niehues.raquel@gmail.com>
2023-06-27 22:10:55 -0300

    Criação dos testes com JUnit.

---
+++ src/test/java/estamparia/CadastroDePedidoTeste.java
@@ -0,0 +1,17 @@
+package estamparia;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.vo.Camisa;
+import model.vo.Pedido;
+
+class CadastroDePedidoTeste {
+
+	@Test
+	public void testCadastroDePedido() {
+		
+	}
+
+}
